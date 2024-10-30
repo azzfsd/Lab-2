748215a21f9abab6db7bef780f47b980c57473a3
Azhad <azhadsbh.23@ichat.sp.edu.sg>
2024-10-30 10:44:07 +0800

    Added bmi.py

---
+++ bmi.py
@@ -0,0 +1,20 @@
+def calculate_bmi(weight, height):
+    print("Height = "+ str(height))
+    print("Weight = "+ str(weight))
+
+    BMI = weight / (height ** 2)
+    print("BMI = ", round(BMI, 2))
+    print("WEight Classification: ", end= "")
+    if BMI < 18.5:
+        print("Under weight")
+    elif BMI > 25.0:
+        print("Over weight")
+    else:
+        print("Normal weight")
+    
+
+calculate_bmi(weight=57,height=1.73)
+print("==========")
+calculate_bmi(weight=97,height=1.73)
+print("==========")
+calculate_bmi(weight=37,height=1.73)
