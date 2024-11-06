34bcedb4faed895c0b582832cbfdafc73e26410b
Azhad <azhadsbh.23@ichat.sp.edu.sg>
2024-11-06 10:57:46 +0800

    Modified bmi.py to return integer

--- bmi.py
+++ bmi.py
@@ -1,20 +1,23 @@
 def calculate_bmi(weight, height):
     print("Height = "+ str(height))
     print("Weight = "+ str(weight))
-
+ 
+    #Calculate the BMI   
     BMI = weight / (height ** 2)
     print("BMI = ", round(BMI, 2))
+
+    #Print BMI
     print("Weight Classification: ", end="")
     if BMI < 18.5:
         print("Under weight")
+        return -1
     elif BMI > 25.0:
         print("Over weight")
+        return 1
     else:
         print("Normal weight")
+        return 0
     
 
-calculate_bmi(weight=57,height=1.73)
-print("==========")
-calculate_bmi(weight=97,height=1.73)
-print("==========")
-calculate_bmi(weight=37,height=1.73)
+result = calculate_bmi(weight=57, height=1.73)
+print("Result = ", result)
